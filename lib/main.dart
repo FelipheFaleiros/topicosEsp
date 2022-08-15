75c6ec8feb968a0176bbcdee5f3a90b2040f0a2e
FlutLab.io <>
2022-08-15 23:26:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Column(
       children: const <Widget>[
-        Text('Deliver features faster', textDirection: TextDirection.ltr,),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
         Expanded(
           child: FittedBox(
             fit: BoxFit.contain, // otherwise the logo will be tiny
