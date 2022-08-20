20050dafc4ca330f36e7b32351a193c8604352ea
FlutLab.io <>
2022-08-20 13:41:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() => runApp(
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
-            child: const Icon(Icons.remove),
+            child: const Icon(Icons.add_a_photo),
           ),
         ),
       ),
