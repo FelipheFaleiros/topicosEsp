971abdebfee80087a3981581bcb13378d3177410
FlutLab.io <>
2022-08-20 13:40:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Transferência'),
-      
-    ),
-  ),
-),);
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Transferência'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed:() {
+
+            }, 
+            child: const Icon(Icons.navigate_before),
+           ),
+        ),
+      ),
+    );
