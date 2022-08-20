7524bf64a7bf23fc7af4fd510f7b44a00f5a3341
FlutLab.io <>
2022-08-20 13:17:26 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: const <Widget>[
-        Text(
-          'Primeiro texto flutter',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Segundo texto flutter',
-          textDirection: TextDirection.ltr,
-        ),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: FlutterLogo(),
-          ),
-        ),
-      ],
-    ));
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Transferência'),
+      
+    ),
+  ),
+),);
